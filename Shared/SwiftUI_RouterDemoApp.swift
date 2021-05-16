683594b167301135e39934e0900d62643cb5d810
史 翔新 <x_shi@yumemi.co.jp>
2021-05-17 01:41:38 +0900

    Init project

---
+++ Shared/SwiftUI_RouterDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_RouterDemoApp.swift
+//  Shared
+//
+//  Created by 史 翔新 on 2021/05/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_RouterDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
